263429cd371a621a608755b48f7115dbcb48cd32
KachuraAlexandr <kachuraalexandr@mail.ru>
2020-05-03 18:47:43 +0200

    Добавлены тесты для макроса NEWLINE.

--- examples/test_gas/test_gas.s
+++ examples/test_gas/test_gas.s
@@ -3,10 +3,14 @@
 .global      _start
 
 _start:
+	OUTSTRLN 	$outstrln_str	# passed the test
+	OUTSTR		$outstr_str		# passed the test 
+	NEWLINE						# passed the test
+	NEWLINE		$5				# passed the test
+	EXIT 			 			# passed the test
 
-	OUTNUM	num
+outstrln_str:
+	.string "This is the OUTSTRLN output."
 
-	EXIT 			 # passed the test
-
-num:
-	.word 1
+outstr_str:
+	.string "This is the OUTSTR output."
