6fbd5cce2b4f1b08e366c089e5a7857290462815
KachuraAlexandr <kachuraalexandr@mail.ru>
2020-07-09 22:53:32 +0200

    Добавлен тест для макроса FLUSH.

--- examples/test_gas/test_gas.s
+++ examples/test_gas/test_gas.s
@@ -3,10 +3,21 @@
 .global      _start
 
 _start:
-	OUTSTRLN 	$outstrln_str	# passed the test
+    OUTSTRLN 	$outstrln_str	# passed the test
+
 	OUTSTR		$outstr_str		# passed the test 
+
 	NEWLINE						# passed the test
 	NEWLINE		$5				# passed the test
+
+    PAUSE
+    PAUSE       $pause_str
+    
+    OUTSTR      $outstr_str
+    FLUSH
+    movl        $0, %eax
+    call        _exit
+
 	EXIT 			 			# passed the test
 
 outstrln_str:
@@ -14,3 +25,6 @@ outstrln_str:
 
 outstr_str:
 	.string "This is the OUTSTR output."
+
+pause_str:
+    .string "This is the PAUSE output.\n"
